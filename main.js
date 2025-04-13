fc0ee28cb3d541f31297c031e0ecc86ddc5f9fe4
Igor Lucas <igorlucasdev@gmail.com>
2025-04-13 10:51:49 -0300

    project setup

---
+++ main.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer');
+
+const bot = mineflayer.createBot({
+    host: 'localhost',
+    username: 'Bee',
+    auth: 'offline',
+    port: '25565',
+    version: '1.20.1'
+})
